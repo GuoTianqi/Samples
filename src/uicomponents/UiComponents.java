4aacc71b182f44e0affc6b6afde94b646009105a
Tianqi Guo <guotianqi1987@gmail.com>
2017-12-12 18:00:06 +0800

    modify uicomponents

--- src/uicomponents/UiComponents.java
+++ src/uicomponents/UiComponents.java
@@ -1,14 +1,18 @@
 package uicomponents;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import resources.ResUtils;
 
@@ -43,10 +47,25 @@ public class UiComponents extends Application {
         Label label1 = new Label();
         Label label2 = new Label("Search");
         Image image = new Image(ResUtils.getImageAsStream("smile.png"));
-        Label label3 = new Label("Search", new ImageView(image));
+        final Label label3 = new Label("Search", new ImageView(image));
+        label3.setContentDisplay(ContentDisplay.RIGHT);
+        label3.setGraphicTextGap(20);
+        label3.setFont(new Font("Consolas", 30));
+        label3.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                label3.setScaleX(1.5);
+                label3.setScaleY(1.5);
+            }
+        });
 
+        label3.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                label3.setScaleX(1);
+                label3.setScaleY(1);
+            }
+        });
         flowPane.getChildren().addAll(label1, label2, label3);
     }
-
-
 }
