f3c60ec98695635ca47671f9128aa362cc5c7964
Guo Tianqi <guotianqi1987@gmail.com>
2018-10-29 15:15:27 +0800

    Modify RxJava WindowMain

--- src/main/java/com/gtq/javasample/rxjava/WindowMain.java
+++ src/main/java/com/gtq/javasample/rxjava/WindowMain.java
@@ -7,9 +7,14 @@ import io.reactivex.functions.Consumer;
 import java.util.concurrent.TimeUnit;
 
 public class WindowMain {
-    public static void main(String[] args) {
-        Observable.interval(1, TimeUnit.SECONDS)
+    public static void main(String[] args) throws InterruptedException {
+        Observable.interval(1500, TimeUnit.MICROSECONDS)
                 .take(15)
-                .window(3);
+                .window(3)
+                .subscribe(longObservable -> {
+                    longObservable.subscribe(aLong -> System.out.println(aLong));
+                });
+
+        Thread.sleep(20000);
     }
 }
