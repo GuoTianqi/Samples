180360012455d25ddee584a4358faf5cbb155a68
guotianqi <guotianqi1987@gmail.com>
2020-05-25 14:35:08 +0800

    add kotlinx.coroutines

--- src/main/kotlin/com/gtq/faith/kotlin/coroutines/Main.kt
+++ src/main/kotlin/com/gtq/faith/kotlin/coroutines/Main.kt
@@ -4,8 +4,26 @@ import kotlinx.coroutines.*
 
 fun main() = runBlocking<Unit> { // start main coroutine
     launch { // launch a new coroutine in background and continue
-        delay(1000L)
-        println("World!")
+        delay(200L)
+        println("Task from runBlocking")
     }
-    println("Hello,") // main coroutine continues here immediately
+
+//    launch { // launch a new coroutine in background and continue
+//        delay(100L)
+//        println("I am Crazy!!")
+//    }
+//
+    coroutineScope {
+        launch {
+            delay(500L)
+            println("Task from nested launch")
+        }
+
+        delay(100L)
+        println("Task from coroutine scope")
+    }
+
+
+    println("Coroutine scope is over")
+
 }
